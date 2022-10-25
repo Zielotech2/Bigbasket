1899aadfabe10a012a682f05424f1e31053f1816
Jyoti ranjan pradhan <munapradhan0@gmail.com>
2022-10-25 12:14:27 +0000

    It's my 4th commit

--- integrate.sh
+++ integrate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-# This code is prepared gor integrating tje other source codes
+# This code is prepared gor integrating tje other source code
+
+cp Product.java product_`date +'%d-%m-%y'`.java
